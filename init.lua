97561958f3421ac241d7cf0af5a71e0b3e25c51d
Michael Neumann <michael_neumann@gmx.net>
2025-04-02 11:56:39 +0200

    add buffer completion

--- init.lua
+++ init.lua
@@ -833,6 +833,7 @@ require('lazy').setup({
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp-signature-help',
     },
@@ -911,6 +912,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'buffer' },
           { name = 'nvim_lsp_signature_help' },
         },
       }
